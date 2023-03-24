bb02d5e1eb280dc7ed200e5ef7d848607a806d05
Shreyash Shantam <shreyverma03@gmail.com>
2023-03-24 00:24:46 -0400

    Movie Seat UI

--- home/forms.py
+++ home/forms.py
@@ -6,8 +6,11 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['seats']
+        labels = {
+            'seats': "Select your Seat Below "
+        }
         widgets = {
-            'seats': forms.NumberInput(attrs={'min': 1, 'max': 10, 'value': 1}),
+            'seats': forms.NumberInput(attrs={'min': 1, 'max': 10, 'value': 1,'id': 'seats-field'}),
         }
 
 
